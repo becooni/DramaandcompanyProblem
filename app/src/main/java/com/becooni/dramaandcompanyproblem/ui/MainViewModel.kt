8cb3fc6c64d16016001224036217a4f2738640a7
becooni <junghum21@gmail.com>
2021-03-04 19:28:23 +0900

    User 검색 API
    - Api 호출
    - data set

--- app/src/main/java/com/becooni/dramaandcompanyproblem/ui/MainViewModel.kt
+++ app/src/main/java/com/becooni/dramaandcompanyproblem/ui/MainViewModel.kt
@@ -1,8 +1,15 @@
 package com.becooni.dramaandcompanyproblem.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.becooni.dramaandcompanyproblem.model.User
 import com.becooni.dramaandcompanyproblem.repository.GithubRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +17,21 @@ class MainViewModel @Inject constructor(
     private val githubRepository: GithubRepository
 ) : ViewModel() {
 
-    
+    private val disposable = CompositeDisposable()
+
+    private val _users = MutableLiveData<List<User>>()
+    val users: LiveData<List<User>> = _users
+
+    fun searchUsers(query: String) {
+        githubRepository.getUsers(query)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    _users.value = it
+                },
+                Throwable::printStackTrace
+            )
+            .addTo(disposable)
+    }
 }
