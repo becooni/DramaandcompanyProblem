77dff489065c601141ee6e3cf09d7c13eda933ff
becooni <junghum21@gmail.com>
2021-03-04 19:13:28 +0900

    Repository 추가
    - Repository
    - Di module
    - ConstonantUtil (초성 추출)

---
+++ app/src/main/java/com/becooni/dramaandcompanyproblem/repository/ConsonantUtil.kt
@@ -0,0 +1,28 @@
+package com.becooni.dramaandcompanyproblem.repository
+
+
+class ConsonantUtil {
+
+    private val initialConsonants = arrayOf(
+        "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ",
+        "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ",
+        "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ",
+        "ㅋ", "ㅌ", "ㅍ", "ㅎ"
+    )
+
+    private fun isHangul(char: Char) = isHangul(char.toInt())
+
+    private fun isHangul(code: Int) = code in 0xAC00..0xD7A3
+
+    fun getInitial(str: String): Char? {
+        val initial = str.firstOrNull() ?: return null
+
+        if (!isHangul(initial)) return initial
+
+        val a = (initial - 0xAC00).toInt()
+
+        val result = ((a - (a % 28)) / 28) / 21
+
+        return initialConsonants[result].first()
+    }
+}
---
+++ app/src/main/java/com/becooni/dramaandcompanyproblem/repository/GithubRepository.kt
@@ -0,0 +1,45 @@
+package com.becooni.dramaandcompanyproblem.repository
+
+import com.becooni.dramaandcompanyproblem.network.GithubClient
+import com.becooni.dramaandcompanyproblem.persistence.UserDao
+import javax.inject.Inject
+
+class GithubRepository @Inject constructor(
+    private val githubClient: GithubClient,
+    private val userDao: UserDao,
+    private val consonantUtil: ConsonantUtil
+) {
+
+    fun getUsers(
+        query: String,
+        page: Int = 0,
+        perPage: Int = 100,
+    ) =
+        githubClient.getUsers(query, page, perPage)
+            .map { it.users }
+            .zipWith(userDao.getBookmarkedUsers()) { users, bookmarkedUsers ->
+                if (bookmarkedUsers.isEmpty()) {
+                    users
+                } else {
+                    val userMap = users.associateBy { it.id }
+                        .toMutableMap()
+
+                    val bookmarkUserMap = bookmarkedUsers.associateBy { it.id }
+
+                    for (bookmarkUser in bookmarkUserMap) {
+                        val key = bookmarkUser.key
+                        if (userMap[key]?.id == bookmarkUser.value.id) {
+                            userMap[key] = bookmarkUser.value
+                        }
+                    }
+
+                    userMap.values.toList()
+                }
+            }
+            .map { list -> list.sortedBy { it.name } }
+            .map { list ->
+                list.map { it.apply { initialConsonant = consonantUtil.getInitial(it.name) } }
+            }
+
+    fun getBookmarkUsers() = userDao.getBookmarkedUsers()
+}
