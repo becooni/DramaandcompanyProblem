1b6302af15914c6c3f899882522b8c05d7332940
becooni <junghum21@gmail.com>
2021-03-04 18:58:08 +0900

    Multidex 및 Hilt 사용을 위한 작업
    - Application 클래스 추가
    - Manifest App 추가

---
+++ app/src/main/java/com/becooni/dramaandcompanyproblem/App.kt
@@ -0,0 +1,7 @@
+package com.becooni.dramaandcompanyproblem
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : MultiDexApplication()
